260b6be4c367372a0c79b5a7e3da2526f51b7e17
obailkeri <bailkeri.omkar@gmail.com>
2020-07-15 23:21:19 +0530

    Updated UserWebsite

--- UserWeb/settings.js
+++ UserWeb/settings.js
@@ -5,7 +5,7 @@ window.onload = function() {myFunction()};
 var navbar = document.getElementById("navbar-id");
 
 function myFunction() {
-    if (window.pageYOffset >= 10) {
+    if (window.pageYOffset >= 0) {
         navbar.classList.add("navbar-light");
         document.getElementById("smaller-border").style.backgroundColor = "#227093";
     } else {
